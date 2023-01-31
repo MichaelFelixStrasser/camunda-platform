aa50b6717a8b6ad446949da925d168313016a365
MichaelFelixStrasser <82045936+MichaelFelixStrasser@users.noreply.github.com>
2023-01-31 22:08:26 +0100

    Added Calc_Prize Worker

--- orchestrate-microservices/worker-java/src/main/java/io/camunda/getstarted/tutorial/Worker.java
+++ orchestrate-microservices/worker-java/src/main/java/io/camunda/getstarted/tutorial/Worker.java
@@ -32,4 +32,44 @@ public class Worker {
       return variables;
   }
 
+    @JobWorker(type = "calc_prize")
+    public Map<String, Object> calcPrize(final ActivatedJob job) {
+
+        // Do the business logic
+        System.out.println("Yeah, now you can orchestrate something :-) You could use data from the process variables: " + job.getVariables());
+
+        // get variables for price calculation
+        Map<String, Object> incomingVariables = job.getVariablesAsMap();
+        int price_modification_factor = (int) incomingVariables.get("chosen_pricemodel");
+        int number_developers = (int) incomingVariables.get("InputData_devcount");
+        String feature_complexity = (String) incomingVariables.get("InputData_com");
+
+
+        // define costs
+        double developer_cost = 1000.0;
+        double complexity_bonus_s = 1000.0;
+        double complexity_bonus_m = 2000.0;
+        double complexity_bonus_l = 3000.0;
+
+
+        // get complexity bonus
+        double final_complexity_bonus = 0.0;
+        if(feature_complexity.equals("S")) {final_complexity_bonus = complexity_bonus_s;}
+        if(feature_complexity.equals("M")) {final_complexity_bonus = complexity_bonus_m;}
+        if(feature_complexity.equals("L")) {final_complexity_bonus = complexity_bonus_l;}
+
+        // price calculation
+        double price_wo_modification = ((number_developers * developer_cost) + final_complexity_bonus);
+        double price_modification = price_wo_modification * (price_modification_factor/100);
+        double finale_price = price_wo_modification + price_modification;
+
+
+
+
+        // Probably add some process variables
+        HashMap<String, Object> variables = new HashMap<>();
+        variables.put("price", finale_price);
+        return variables;
+    }
+
 }
